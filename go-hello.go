7ae5b16286a398dc84e31108441375abecd7db3d
mynz <rollingteapot@gmail.com>
2016-12-06 10:41:01 +0900

    add my-httpd.go

--- go-hello.go
+++ go-hello.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"time"
+	// "sync"
 )
 
+func hoge(d time.Duration) <-chan time.Time {
+	fmt.Println("call hoge")
+	return time.After(d)
+}
+
+func fuga() chan bool {
+	ch := make(chan bool)
+	fmt.Println("call fuga")
+	return ch
+}
+
 func main() {
-	fmt.Println("hell world!")
+	fmt.Println("hello")
+	// fmt.Println("NumGroutine", runtime.NumGoroutine())
+
+	fmt.Println("NumCPU", runtime.NumCPU())
+
+	select {
+	// case c := <-time.After(time.Second):
+	case c := <-hoge(time.Second):
+		fmt.Println("one:", c)
+	case <-fuga():
+		fmt.Println("two:")
+	}
+
+	fmt.Println("finish.")
 }
