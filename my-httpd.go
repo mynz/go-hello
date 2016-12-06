d7aa393ed73b1a39543f1db0b2523d8517b7c37e
mynz <rollingteapot@gmail.com>
2016-12-06 12:42:39 +0900

    try fib

--- my-httpd.go
+++ my-httpd.go
@@ -1,4 +1,33 @@
 package main
 
+import "fmt"
+
+// import "log"
+
+func fib() func() int {
+	a, b := 0, 0
+	// a, b := 0, 1
+	// a, b := 1, 0
+
+	return func() int {
+
+		ret := a + b
+
+		a, b = ret, a
+
+		if b == 0 {
+			b = 1
+		}
+
+		return ret
+	}
+}
+
 func main() {
+
+	fn := fib()
+	for i := 0; i < 5; i++ {
+		fmt.Println("fib: ", fn())
+	}
+
 }
