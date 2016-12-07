b8e82c4cfa0d1bf110917c9f42e6bd14f0a99c5a
mynz <rollingteapot@gmail.com>
2016-12-07 18:15:06 +0900

    json

--- try-json.go
+++ try-json.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Message struct {
+	Name string
+}
+
 func main() {
+	mes := Message{Name: "mynz"}
+
+	doc := map[string]Message{
+		"jaga": {"jagajin"},
+	}
+
+	doc["mynz"] = mes
+	doc["am"] = Message{"morimoto"}
+
+	bs, err := json.Marshal(doc)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(len(bs))
+	// fmt.Println(bs)
+	fmt.Println(string(bs))
 
 }
