76ce99095b98a18c9c0a0875dc309229504fec96
mynz <rollingteapot@gmail.com>
2016-12-08 11:40:21 +0900

    json

--- try-json.go
+++ try-json.go
@@ -5,27 +5,67 @@ import (
 	"fmt"
 )
 
-type Message struct {
-	Name string
+type Member struct {
+	Name string `json:"name"`
+}
+
+type Group struct {
+	GroupName string
+	Members   []Member
 }
 
 func main() {
-	mes := Message{Name: "mynz"}
 
-	doc := map[string]Message{
-		"jaga": {"jagajin"},
+	g := Group{
+		"the team",
+		[]Member{
+			Member{"mynz"},
+			Member{"jagajin"},
+		},
 	}
 
-	doc["mynz"] = mes
-	doc["am"] = Message{"morimoto"}
+	doc := struct {
+		Group Group
+	}{
+		g,
+	}
+
+	// fmt.Printf("g: %T\n", g)
+	// fmt.Printf("doc: %T\n", doc)
+	// fmt.Printf("doc: %T, %v", doc, doc)
 
 	bs, err := json.Marshal(doc)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(len(bs))
-	// fmt.Println(bs)
 	fmt.Println(string(bs))
 
 }
+
+/*
+ * func main() {
+ *     mem := Member{Name: "mynz"}
+ *
+ *     group := Group{Members{
+ *         Member{"terimukuri"},
+ *     }}
+ *
+ *     doc := map[string]Member{
+ *         "jaga": {"jagajin"},
+ *     }
+ *
+ *     doc["mynz"] = mem
+ *     doc["am"] = Member{"morimoto"}
+ *
+ *     bs, err := json.Marshal(doc)
+ *     if err != nil {
+ *         panic(err)
+ *     }
+ *
+ *     // fmt.Println(len(bs))
+ *     // fmt.Println(bs)
+ *     fmt.Println(string(bs))
+ *
+ * }
+ */
