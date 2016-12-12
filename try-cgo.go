388d2c967522b6cece589fcb3be2d274a684d64c
mynz <rollingteapot@gmail.com>
2016-12-12 17:56:44 +0900

    cgo works

--- try-cgo.go
+++ try-cgo.go
@@ -3,15 +3,26 @@ package main
 /*
 
 #include <stdio.h>
+#include <stdlib.h>
 
-void Hoge(){
-	puts("hoge")
+int Hoge(){
+	puts("hoge");
+	fflush(stdout);
+	return 777;
 }
 
 */
-
 import "C"
+import "fmt"
 
 func main() {
+	fmt.Println("hello")
 
+	ret := C.Hoge()
+	fmt.Printf("ret: %T, %v\n", ret, ret)
+
+	v := C.random()
+	fmt.Println("v:", v)
+
+	fmt.Println("bye!")
 }
